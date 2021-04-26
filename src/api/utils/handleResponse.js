89dfb61599f63eef7cb15807a8728bee52279320
subject-21 <fefenaim13@gmail.com>
2021-04-26 20:11:31 +0300

    add test data to reporter -jasmine

--- src/api/utils/handleResponse.js
+++ src/api/utils/handleResponse.js
@@ -1,8 +1,11 @@
 const TestError = require("./testError")
+const Reporter = require("../../../jasmineHelper").allure
 /** @returns {Promise<import("axios").AxiosResponse>} */
 const handleResponse = async (axiosInstance, method, args) => {
     try {
-        return await (axiosInstance[method].apply(this, args));
+        const requestAction = await (axiosInstance[method].apply(this, args)); 
+        Reporter.description(JSON.stringify(requestAction.config));
+        return (requestAction);
     } catch (err) {
         if (!err.isAxiosError) {
             throw new TestError(err.message);
